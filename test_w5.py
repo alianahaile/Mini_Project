2995d12046d8a4d959530c13131cd3a64890516c
Aliana Haile <67199470+alianahaile@users.noreply.github.com>
2021-08-27 13:59:55 +0100

    Delete test_w5.py

--- test_w5.py
+++
@@ -1,8 +0,0 @@
-# from prompt_toolkit import print_formatted_text, HTML
-
-
-
-
-# print_formatted_text(HTML('<b> Aliana </b>'))
-# print_formatted_text(HTML('<i> Oussama </i>'))
-# print_formatted_text(HTML('<u> <b> Rumaanah </b </u>'))
